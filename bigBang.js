26dea9030ffe486fb189b5182fc3338f638ba437
jie669928 <138839269+jie669928@users.noreply.github.com>
2024-03-30 19:49:55 +0800

    Add files via upload

---
+++ bigBang.js
@@ -0,0 +1,24 @@
+function generateBigBangArray() {
+    const bigBangArray = [];
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            bigBangArray.push("BIG BANG");
+        } else if (i % 3 === 0) {
+            bigBangArray.push("BIG");
+        } else if (i % 5 === 0) {
+            bigBangArray.push("BANG");
+        } else {
+            bigBangArray.push(`${i}`);
+        }
+    }
+    return bigBangArray;
+}
+
+// Save the array to an output.json file
+const fs = require('fs');
+const result = generateBigBangArray();
+
+fs.writeFile('output.json', JSON.stringify(result, null, 2), (err) => {
+    if (err) throw err;
+    console.log('The BIG BANG array has been saved to output.json');
+});
